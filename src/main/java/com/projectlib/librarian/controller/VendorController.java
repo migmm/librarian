2252317a54c8e489b615cbf1582bd658b5de818a
migmm <micmigm@gmail.com>
2023-10-29 23:21:18 -0300

    Created vendor controller

--- src/main/java/com/projectlib/librarian/controller/VendorController.java
+++ src/main/java/com/projectlib/librarian/controller/VendorController.java
@@ -1,4 +1,43 @@
 package com.projectlib.librarian.controller;
 
+import com.projectlib.librarian.model.Vendor;
+import com.projectlib.librarian.service.VendorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/vendors")
 public class VendorController {
+    @Autowired
+    private VendorService vendorService;
+
+    @GetMapping("/findAll")
+    public List<Vendor> getAllVendors() {
+        return vendorService.getAllVendors();
+    }
+
+    @GetMapping("/find/{id}")
+    public Vendor getVendorById(@PathVariable Long id) {
+        return vendorService.getVendorById(id);
+    }
+
+    @PostMapping("/save")
+    public Vendor createVendor(@RequestBody Vendor vendor) {
+        return vendorService.createVendor(vendor);
+    }
+
+    @PutMapping("/update/{id}")
+    public Vendor updateVendor(@PathVariable Long id, @RequestBody Vendor vendor) {
+        return vendorService.updateVendor(id, vendor);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteVendor(@PathVariable Long id) {
+        vendorService.deleteVendor(id);
+    }
 }
