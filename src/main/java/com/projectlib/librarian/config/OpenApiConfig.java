1870c634936abb49004f9d5d3aa5df1c1978683e
migmm <micmigm@gmail.com>
2023-11-03 18:57:22 -0300

    Added information to Swagger documentation

---
+++ src/main/java/com/projectlib/librarian/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.projectlib.librarian.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Librarian Spring Boot API")
+                        .version("1")
+                        .description("Librarian Spring Boot 3 API endpoints.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+}
