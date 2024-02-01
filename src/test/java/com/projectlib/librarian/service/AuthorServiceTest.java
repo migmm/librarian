96337562f12228b8d0c3ab213b99ea49a615b4dc
Miguel <miguelm@hotmail.com.ar>
2024-02-01 20:22:39 -0300

    Created author service test

--- src/test/java/com/projectlib/librarian/service/AuthorServiceTest.java
+++ src/test/java/com/projectlib/librarian/service/AuthorServiceTest.java
@@ -1,89 +1,147 @@
 package com.projectlib.librarian.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.projectlib.librarian.dto.AuthorDTO;
+import com.projectlib.librarian.exception.NotFoundException;
 import com.projectlib.librarian.model.Author;
 import com.projectlib.librarian.repository.AuthorRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.mockito.Mockito.when;
-
-import java.util.HashSet;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 @SpringBootTest
-@DisplayName("Author service tests")
-public class AuthorServiceTest {
+class AuthorServiceTest {
+
+    @InjectMocks
+    private AuthorImplementation authorService;
 
     @Mock
     private AuthorRepository authorRepository;
 
-    @InjectMocks
-    private AuthorInterface authorInterface;
+    @Test
+    void findAllAuthorsTest() {
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+        Author author1 = mock(Author.class);
+        Author author2 = mock(Author.class);
+        when(authorRepository.findAll()).thenReturn(Arrays.asList(author1, author2));
+
+        List<AuthorDTO> result = authorService.getAllAuthors();
+
+        assertEquals(2, result.size());
     }
 
     @Test
-    @DisplayName("Create an author")
-    public void testCreateAuthor() {
-        Author newAuthor = new Author(1L, "New Author", "New Last Name", true, new HashSet<>());
+    void findAllITest() {
 
-        when(authorRepository.save(Mockito.any(Author.class))).thenReturn(newAuthor);
+        Author author1 = new Author();
+        author1.setId(1L);
+        author1.setName("John");
+        author1.setSurname("Doe");
+        author1.setStatus(true);
 
-        String message = authorInterface.createAuthor(newAuthor);
+        Author author2 = new Author();
+        author2.setId(2L);
+        author2.setName("Jane");
+        author2.setSurname("Smith");
+        author2.setStatus(true);
 
-        assertEquals("Author created successfully.", message);
+        List<Author> authorList = Arrays.asList(author1, author2);
+        when(authorRepository.findAll()).thenReturn(authorList);
+
+        List<AuthorDTO> expected = authorList.stream()
+                .map(author -> new AuthorDTO(author.getId(), author.getName(), author.getSurname(), author.getStatus()))
+                .collect(Collectors.toList());
+
+        List<AuthorDTO> result = authorService.getAllAuthors();
+
+        assertNotNull(result);
+        assertEquals(expected.size(), result.size());
+
+        for (int i = 0; i < expected.size(); i++) {
+            AuthorDTO expectedDTO = expected.get(i);
+            AuthorDTO resultDTO = result.get(i);
+            assertEquals(expectedDTO.getId(), resultDTO.getId());
+            assertEquals(expectedDTO.getName(), resultDTO.getName());
+            assertEquals(expectedDTO.getSurname(), resultDTO.getSurname());
+            assertEquals(expectedDTO.getStatus(), resultDTO.getStatus());
+        }
     }
 
     @Test
-    @DisplayName("Update an author")
-    public void testUpdateAuthor() {
+    void saveAuthorTest() {
+
+        Author author = mock(Author.class);
+        AuthorDTO authorDto = mock(AuthorDTO.class);
+        when(authorRepository.save(any(Author.class))).thenReturn(author);
+
+        String result = authorService.createAuthor(authorDto);
+
+        assertEquals("Author created successfully.", result);
+    }
+
+    @Test
+    void deleteAuthorTest() {
+
         Long authorId = 1L;
-        Author existingAuthor = new Author(authorId, "Author 1", "Last Name 1", true, new HashSet<>());
-        Author updatedAuthor = new Author(authorId, "Updated Author", "Updated Last Name", false, new HashSet<>());
+        doNothing().when(authorRepository).deleteById(authorId);
 
-        when(authorRepository.findById(authorId)).thenReturn(Optional.of(existingAuthor));
-        when(authorRepository.save(Mockito.any(Author.class))).thenReturn(updatedAuthor);
+        String result = authorService.deleteAuthor(authorId);
 
-        String message = authorInterface.updateAuthor(authorId, updatedAuthor);
-
-        assertEquals("Author updated successfully.", message);
+        assertEquals("Author with ID " + authorId + " deleted successfully.", result);
     }
 
     @Test
-    @DisplayName("Set status (logical deletion)")
-    public void testSetStatus() {
+    void getAuthorByIdTest() {
+
         Long authorId = 1L;
-        Author existingAuthor = new Author(authorId, "Author 1", "Last Name 1", true, new HashSet<>());
-        Author updatedAuthor = new Author(authorId, "Author 1", "Last Name 1", false, new HashSet<>());
+        Author author = new Author();
+        author.setId(authorId);
+        when(authorRepository.findById(authorId)).thenReturn(Optional.of(author));
 
-        when(authorRepository.findById(authorId)).thenReturn(Optional.of(existingAuthor));
-        when(authorRepository.save(Mockito.any(Author.class))).thenReturn(updatedAuthor);
+        AuthorDTO result = authorService.getAuthorById(authorId);
 
-        String message = authorInterface.setStatus(authorId, updatedAuthor);
-
-        assertEquals("Author status updated successfully.", message);
+        assertEquals(authorId, result.getId());
     }
 
     @Test
-    @DisplayName("Delete author")
-    public void testDeleteAuthor() {
+    void getAuthorByIdNotFoundTest() {
+
         Long authorId = 1L;
+        when(authorRepository.findById(authorId)).thenReturn(Optional.empty());
 
-        when(authorRepository.existsById(authorId)).thenReturn(true);
+        assertThrows(NotFoundException.class, () -> authorService.getAuthorById(authorId));
+    }
 
-        String message = authorInterface.deleteAuthor(authorId);
+    @Test
+    void updateAuthorNotFoundTest() {
 
-        assertEquals("Author with ID 1 deleted successfully.", message);
+        Long authorId = 1L;
+        AuthorDTO updatedAuthorDTO = new AuthorDTO();
+        updatedAuthorDTO.setId(authorId);
+
+        assertThrows(NotFoundException.class, () -> authorService.updateAuthor(authorId, updatedAuthorDTO));
+    }
+
+    @Test
+    void deleteAuthorNotFoundTest() {
+
+        Long authorId = 1L;
+        doThrow(new NotFoundException("Author with ID " + authorId + " does not exist.")).when(authorRepository)
+                .deleteById(authorId);
+
+        assertThrows(NotFoundException.class, () -> authorService.deleteAuthor(authorId));
     }
 }
