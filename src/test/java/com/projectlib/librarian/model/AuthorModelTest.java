a4406350d722d20747193a4acb043d29a200eb8d
Miguel <miguelm@hotmail.com.ar>
2024-02-01 20:19:59 -0300

    Created author model test

--- src/test/java/com/projectlib/librarian/model/AuthorModelTest.java
+++ src/test/java/com/projectlib/librarian/model/AuthorModelTest.java
@@ -1,6 +1,5 @@
 package com.projectlib.librarian.model;
 
-import com.projectlib.librarian.model.Author;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
