1a93b032ff3bb28d8b4100610d926a5c9035eeb4
Miguel <miguelm@hotmail.com.ar>
2024-01-31 21:53:20 -0300

    Moved validatons to DTO

--- src/main/java/com/projectlib/librarian/dto/BookDTO.java
+++ src/main/java/com/projectlib/librarian/dto/BookDTO.java
@@ -2,10 +2,13 @@ package com.projectlib.librarian.dto;
 
 import com.projectlib.librarian.model.Vendor;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.List;
@@ -16,16 +19,51 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookDTO {
+
+    @Schema(description = "Book ID")
     private Long id;
+
+    @NotNull(message = "ISBN is required.")
+    @Digits(integer = 13, fraction = 0, message = "ISBN must contain exactly 13 digits.")
+    @Positive(message = "ISBN must be a positive integer.")
+    @Schema(description = "Book ISBN")
     private Long ISBN;
+
+    @NotBlank(message = "Book title is required.")
+    @Size(min = 2, max = 100, message = "Book title must be between 2 and 100 characters long.")
+    @Schema(description = "Book title")
     private String title;
+
+    @NotNull(message = "Book year is required.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Schema(description = "Book year")
     private Date year;
+
+    @NotNull(message = "Book quantity is required.")
+    @Positive(message = "Book quantity must be a positive integer.")
+    @Schema(description = "Book quantity")
     private Integer books_quantity;
+
+    @Min(value = 0, message = "Borrowed books quantity must be zero or a positive integer.")
+    @Schema(description = "Borrowed books quantity")
     private Integer borrowed_books;
+
+    @Positive(message = "Book quantity must be a positive integer.")
+    @Schema(description = "Book quantity left")
     private Integer books_left;
+
+    @NotBlank(message = "Book genre is required.")
+    @Size(min = 2, max = 100, message = "Book genre must be between 2 and 100 characters long.")
+    @Schema(description = "Book genre")
     private String genre;
+
+    @Schema(description = "Book status")
     private Boolean status;
+
+    @Schema(description = "Book images")
     private List<String> images;
+
+    @Schema(description = "Book vendor")
     private Vendor vendor;
 
     public Boolean getStatus() {
--- src/main/java/com/projectlib/librarian/model/Book.java
+++ src/main/java/com/projectlib/librarian/model/Book.java
@@ -3,12 +3,11 @@ package com.projectlib.librarian.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.HashSet;
@@ -22,6 +21,7 @@ import java.util.Set;
 @Getter
 @Setter
 public class Book {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
@@ -30,42 +30,30 @@ public class Book {
 
     @Column
     @Schema(description = "Book ISBN")
-    @NotNull(message = "ISBN is required.")
-    @Digits(integer = 13, fraction = 0, message = "ISBN must contain exactly 13 digits.")
-    @Positive(message = "ISBN must be a positive integer.")
     private Long ISBN;
 
     @Column
     @Schema(description = "Book title")
-    @NotBlank(message = "Book title is required.")
-    @Size(min = 2, max = 100, message = "Book title must be between 2 and 100 characters long.")
     private String title;
 
     @Column
     @Schema(description = "Book year")
-    @NotNull(message = "Book year is required.")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date year;
 
     @Column
     @Schema(description = "Book quantity")
-    @NotNull(message = "Book quantity is required.")
-    @Positive(message = "Book quantity must be a positive integer.")
     private Integer books_quantity;
 
     @Column
     @Schema(description = "Borrowed books quantity")
-    @Min(value = 0, message = "Borrowed books quantity must be zero or a positive integer.")
     private Integer borrowed_books = 0;
 
     @Column
     @Schema(description = "Book quantity left")
-    @Positive(message = "Book quantity must be a positive integer.")
     private Integer books_left;
 
     @Column
-    @NotBlank(message = "Book genre is required.")
-    @Size(min = 2, max = 100, message = "Book genre must be between 2 and 100 characters long.") @Schema(description = "Book genre")
+    @Schema(description = "Book genre")
     private String genre;
 
     @Column
@@ -75,6 +63,7 @@ public class Book {
     @ElementCollection
     @CollectionTable(name = "book_images", joinColumns = @JoinColumn(name = "book_id"))
     @Column(name = "image_path")
+    @Schema(description = "Book images")
     private List<String> images;
 
     @ManyToOne
