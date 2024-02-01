d96199a1c06fc3356c8922217a54e1504dea7ab8
Miguel <miguelm@hotmail.com.ar>
2024-01-31 21:41:27 -0300

    Moved validations to DTO

--- src/main/java/com/projectlib/librarian/dto/AuthorDTO.java
+++ src/main/java/com/projectlib/librarian/dto/AuthorDTO.java
@@ -1,5 +1,8 @@
 package com.projectlib.librarian.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
@@ -11,9 +14,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthorDTO {
+
+    @Schema(description = "Author ID")
     private Long id;
+
+    @Schema(description = "Author name")
+    @NotBlank(message = "Name is required.")
+    @Size(min = 2, message = "Name must be at least 2 characters long.")
+    @Size(max = 100, message = "Name cannot exceed 100 characters.")
     private String name;
+
+    @Schema(description = "Author surname")
+    @NotBlank(message = "Surname is required.")
+    @Size(min = 2, message = "Surname must be at least 2 characters long.")
+    @Size(max = 100, message = "Surname cannot exceed 100 characters.")
     private String surname;
+
+    @Schema(description = "Author status")
     private Boolean status = true;
 
     public Boolean getStatus() {
--- src/main/java/com/projectlib/librarian/model/Author.java
+++ src/main/java/com/projectlib/librarian/model/Author.java
@@ -3,9 +3,7 @@ package com.projectlib.librarian.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,16 +27,12 @@ public class Author {
 
     @Column
     @Schema(description = "Author name")
-    @NotBlank(message = "Name is required.")
-    @Size(min = 2, message = "Name must be at least 2 characters long.")
-    @Size(max = 100, message = "Name cannot exceed 100 characters.")
+
     private String name;
 
     @Column
     @Schema(description = "Author surname")
-    @NotBlank(message = "Surname is required.")
-    @Size(min = 2, message = "Surname must be at least 2 characters long.")
-    @Size(max = 100, message = "Surname cannot exceed 100 characters.")
+
     private String surname;
 
     @Column
