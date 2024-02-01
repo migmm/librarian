b3219f5675b9ecd8bea4be2535eda6bcf7ab39d4
Miguel <miguelm@hotmail.com.ar>
2024-01-31 21:59:18 -0300

    Moved validations to DTO

--- src/main/java/com/projectlib/librarian/dto/VendorDTO.java
+++ src/main/java/com/projectlib/librarian/dto/VendorDTO.java
@@ -1,5 +1,8 @@
 package com.projectlib.librarian.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
@@ -11,8 +14,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDTO {
+
+    @Schema(description = "Vendor ID")
     private Long id;
+
+    @NotBlank(message = "Vendor name is required.")
+    @Size(min = 2, max = 30, message = "Vendor name must be between 2 and 30 characters long.")
+    @Schema(description = "Vendor name")
     private String name;
+
+    @Schema(description = "Vendor status")
     private Boolean status = true;
 
     public Boolean getStatus() {
--- src/main/java/com/projectlib/librarian/model/Vendor.java
+++ src/main/java/com/projectlib/librarian/model/Vendor.java
@@ -3,8 +3,7 @@ package com.projectlib.librarian.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +19,7 @@ import java.util.Set;
 @Getter
 @Setter
 public class Vendor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "vendor_id")
@@ -28,8 +28,6 @@ public class Vendor {
 
     @Column
     @Schema(description = "Vendor name")
-    @NotBlank(message = "Vendor name is required.")
-    @Size(min = 2, max = 30, message = "Vendor name must be between 2 and 30 characters long.")
     private String name;
 
     @Column
